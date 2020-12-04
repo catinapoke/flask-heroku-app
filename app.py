f61c52ed047b25f3302251a8ce27b10d70779224
DaBoi <mihansp1@gmail.com>
2020-12-04 14:51:27 +0300

    Connected module of finding words to Flask

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
 # flask-heroku-app
+from words_api import *
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World, that Flask app for testing Heroku hosting!'
+    return render_template("index.html",
+                           title="LOTR")
+
+
+@app.route('/sentences', methods=['GET', 'POST'])
+def find_sentences():
+    words = request.form.get('words')  # запрос к данным формы
+    examples_number = request.form.get('examples')
+    sentences = get_sentences_from_file('lotr.txt')
+    correct_sentences = find_words(sentences, words, int(examples_number))['words']
+    print(correct_sentences)
+
+    return render_template('sentences.html',
+                           sentences=correct_sentences)
 
 
 @app.route('/<custom_url>')
 def custom_route(custom_url):
     return f'You at sub-page {custom_url}'
+
+
+@app.route('/request')
+def json_request():
+    # двоичное представления
+    data = request.data
+    print(data)
+    # json
+    result = {
+        "word": json_data["word"],
+        "from": json_data["from"],
+        "to": json_data["to"],
+        "example": [
+            "2222222222222222222222222222222",
+            "33333333333333333333333333333533"
+        ]
+    }
+    return jsonify(result)
 
 
 if __name__ == '__main__':
---
+++ words_api.py
@@ -0,0 +1,37 @@
+from nltk import sent_tokenize
+from typing import List, Dict
+import json
+
+
+def read_request_json(filename: str = "request.json") -> dict:
+    with open(filename) as file:
+        f = json.load(file)
+        return f
+
+
+def get_sentences_from_file(filename: str, lang: str = "english") -> List[str]:
+    with open(filename, "r", encoding="utf-8") as book:
+        text = book.read()
+        sentences = sent_tokenize(text, lang)
+        return sentences
+
+
+def find_words(sentences: List[str], words: List[str], examples: int, min_len: int = 2, max_len: int = 20) -> dict:
+    ret = []
+    for sentence in sentences:
+        if not (min_len <= len(sentence) <= max_len):
+            continue
+        if examples <= 0:
+            break
+        if any(word in sentence for word in words):
+            ret.append(sentence)
+            examples -= 1
+    return {'words': ret}
+
+
+def save_response(obj: dict) -> None:
+    with open("response.json", "w", encoding="utf-8") as file:
+        json.dump(obj, file, ensure_ascii=True, indent=4)
+
+
+
